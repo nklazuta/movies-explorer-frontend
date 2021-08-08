6b8e27e1be7e7b5df98842697f02fdef9bb148e0
Надежда  Лазута <nadezdalazuta@MacBook-Air-Nadezda.local>
2021-08-08 20:06:19 +0300

    feat: добавить стили компоненту PageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -5,10 +5,12 @@ import "./PageNotFound.css";
 export default function PageNotFound() {
   return (
     <div className="not-found">
+      <div className="not-found__text-box">
       <h3 className="not-found__title">
         404
       </h3>
       <p className="not-found__text">Страница не найдена</p>
+      </div>
       <Link to="/" className="not-found__button-to-main">
         Назад
       </Link>
